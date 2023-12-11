2e5ee93aae1ac71eb085174bfe7f5cf9a209626f
willykuheleloa <willy_kuheleloa@yahoo.com>
2023-12-10 20:28:11 -0800

    forgot to add comments as instructed

--- DifferenceArraysArrayLists.java
+++ DifferenceArraysArrayLists.java
@@ -3,18 +3,28 @@ import java.util.Arrays;
 
 public class DifferenceArraysArrayLists {
     public static void main(String[] args) {
-        // Array example
-        String[] colorsArray = new String[2];
-        colorsArray[0] = "Red";
-        colorsArray[1] = "Blue";
+        // demonstration with an array
+        String[] fruitsArray = new String[3]; // Array with fixed size
+        fruitsArray[0] = "Apple";
+        fruitsArray[1] = "Banana";
+        // adding more than 3 elements requires creating a new array
 
-        // ArrayList example
-        ArrayList<String> colorsList = new ArrayList<>();
-        colorsList.add("Red");
-        colorsList.add("Blue");
-        colorsList.add("Green");
+        // uncomment below to see error
+        // fruitsArray[3] = "Mango"; // Causes ArrayIndexOutOfBoundsException 
 
-        System.out.println("Array: " + Arrays.toString(colorsArray));
-        System.out.println("ArrayList: " + colorsList);
+        System.out.println("Array of Fruits: " + Arrays.toString(fruitsArray));
+
+        // demonstration with an ArrayList
+        ArrayList<String> fruitsList = new ArrayList<>(); 
+        //adding to dynamic array
+        fruitsList.add("Apple");
+        fruitsList.add("Banana");
+        fruitsList.add("Cherry"); 
+        fruitsList.add("Mango"); 
+
+        // removing element from array
+        fruitsList.remove("Cherry");
+
+        System.out.println("ArrayList of Fruits: " + fruitsList);
     }
 }
